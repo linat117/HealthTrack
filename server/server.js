f142e292562e68ef4c9cd774505e94d2e264d3ef
Hilina Teshome <teshilina8@gmail.com>
2025-11-01 15:41:34 +0300

    Relation ship between user and advisory established and models tested well

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const dotenv = require("dotenv");
 const app = express();
 const connectDB = require("./config/db");
 const testRoutes = require("./routes/testRoutes");
-
+const Advisory = require("./models/Advisory");
+const User = require("./models/User");
 dotenv.config();
 connectDB();
 app.use(cors());
@@ -13,6 +14,23 @@ app.use("/api/test/", testRoutes);
 
 app.get("/", (req, res) => {
   res.send("Healthlink API is running...");
+});
+
+app.get("/api/test-models", async (req, res) => {
+  const user = await User.create({
+    name: "Test User",
+    email: "test@user.com",
+    password: "12345678",
+  });
+  const advisory = await Advisory.create({
+    title: "Covid 19 alert",
+    description: "There has been a rise in local covid 19 cases.",
+    category: "Outbreak Alert",
+    localtion: "Addis Ababa",
+    postedBy: user._id,
+  });
+
+  res.json({ user, advisory });
 });
 
 const PORT = process.env.PORT || 5000;
