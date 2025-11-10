86b70daa8df0d39a0e69bd76f63842a0a2b7d7f8
Hilina Teshome <teshilina8@gmail.com>
2025-11-10 21:41:58 +0300

    Progect restarted:(

--- server/middleware/adminMiddleware.js
+++
@@ -1,6 +0,0 @@
-export default function adminMiddleware(req, res, next) {
-  if (!req.user || !req.user.isAdmin) {
-    return res.status(403).json({ msg: "Access denied. Admins only." });
-  }
-  next();
-}
